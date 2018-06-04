1015174bf139d4ab1e5827836fcbff699c2b5245
Oleksii Havrylov <olhavrylov@luxoft.com>
2018-06-04 13:03:29 +0300

    Added unittests for Numbers class

--- src/test/java/NumbersTest.java
+++ src/test/java/NumbersTest.java
@@ -1,16 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class NumbersTest {
 
-
-    public void sum() throws Exception{
-            Numbers numbers = new Numbers();
-            int actual = numbers.sum(5);
-            int expected = 15;
-            }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
+    @Test
+    public void sum() {
+        Numbers numbers = new Numbers();
+        int actual = numbers.sum(5);
+        int expected = 15;
+        assertEquals(expected,actual);
     }
-
 }
