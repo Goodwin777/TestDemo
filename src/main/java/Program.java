effd79358499319fc8f380bde8870abb5168a588
Oleksii Havrylov <olhavrylov@luxoft.com>
2018-06-04 12:37:52 +0300

    Added src folder

---
+++ src/main/java/Program.java
@@ -0,0 +1,7 @@
+ public class Program {
+    public static void main(String[]arg){
+        Numbers numbers = new Numbers();
+        int result = numbers.sum(5);
+        System.out.println(result);
+    }
+}
